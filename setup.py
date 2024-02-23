05149a43078d35135ed0a5ddced50d930e9a667c
RoyNisimov <roynisimov@gmail.com>
2024-02-23 15:57:10 +0200

    Prepering to publish

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+DESC = 'A simple asymmetric encryption module'
+with open("README.md", 'r') as f:
+    LONG_DESC = f.read()
+setup(
+    name="",
+    version="0.0.1",
+    author="Roy Nisimov, jacebalaron (Daniel gaisenberg)",
+    description=DESC,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESC,
+    packages=find_packages(),
+    url='https://github.com/RoyNisimov/AsymmetricEncryption',
+    license='MIT',
+    install_requires=[],
+    keywords=['python', 'cipher', 'asymmetric encryptions', 'signing', 'verifying', 'protocols', 'encryption', 'decryption'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
